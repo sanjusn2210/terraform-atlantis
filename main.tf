6b5e3f891a117bd0ddae38d60d11d829ee6350a7
Sanjay S N <114326599+sanjusn2210@users.noreply.github.com>
2025-07-20 12:40:23 +0530

    Update main.tf
    demo

--- main.tf
+++ main.tf
@@ -3,22 +3,21 @@ region = "ap-south-1"
 profile = "default"
 
 }
-
-
 terraform {
-backend "s3" {
-bucket = "mycompany-terraform-backend123"
-key = "atlantis/terraform.tfstate"
-region = "us-east-1"
-profile = "default"
+  backend "remote" {
+    organization = "sanjay-dev"
 
+    workspaces {
+      name = "terraform-atlantis"
+    }
+  }
 }
-}
+
 resource "aws_instance" "web" {
-ami = "ami-0a1235697f4afa8a4" # Replace with a valid AMI ID
+ami = "ami-0a1235697f4afa8a4" 
 instance_type = "t2.micro"
 
 tags = {
-Name = "terraform-atlantis"
+Name = "terraform-atlantis-cloud"
 }
 }
